9e974611a0e1f1b145524b0079874d338a985029
rajender38 <rajenderameelpur@gmail.com>
2021-01-11 19:16:21 +1000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComputerTypeComponent } from "../components/computer-type/computer-type.component";
+import { ComputerListComponent } from "../components/computer-list/computer-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: `computer-type`,
+    component: ComputerTypeComponent,
+},
+{
+  path: `computer-list`,
+  component: ComputerListComponent,
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
